88f59a969d85d6788668dab3ac8c9d4d3fa5bbe7
gawadevibhutics242558-jpg <gawadevibhutics242558@gmail.com>
2025-12-08 12:10:29 +0530

    Add files via upload

--- VIBHUTI_PRAC13.R
+++ VIBHUTI_PRAC13.R
@@ -1,41 +1,23 @@
 
-
-
-
 library(dplyr)
-
-
-# 1. READ YOUR WINE DATA
 
 wine_df <- read.csv("D:/S079_VIBHUTI/ADV PYTHON FOR DATA SCIENCE/winequality-red.csv")
 
 print("--- 1. Original Wine Dataset ---")
 print(head(wine_df))
 
-
-# 2. IDENTIFYING DUPLICATES 
-
 duplicates_report <- wine_df %>%
-  group_by(across(everything())) %>%  # group by ALL columns
-  count() %>%                          # count duplicates
-  filter(n > 1)                        # keep only duplicates
+  group_by(across(everything())) %>%  
+  count() %>%                         
+  filter(n > 1)                       
 
 print("--- 2. Rows that are duplicated (Full duplicate report) ---")
 print(duplicates_report)
 
-
-# 3. REMOVING EXACT DUPLICATE ROWS
-
 clean_exact <- wine_df %>%
-  distinct()     # removes full duplicate rows
-
+  distinct()   
 print("--- 3. Dataset After Removing Exact Duplicates ---")
 print(clean_exact)
-
-
-# 4. HANDLING DUPLICATES BASED ON ONE COLUMN (Example: quality)
-
-
 
 unique_quality <- wine_df %>%
   distinct(quality, .keep_all = TRUE)
